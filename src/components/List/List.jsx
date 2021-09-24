f8eb0e123712145f1045d11f78a8350de05fc45b
Vadim Popov <65708779+senner-code@users.noreply.github.com>
2021-09-24 20:22:24 +0300

    Added sort by click to category/ optimized list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -9,73 +9,88 @@ import sortData from "../../service/service.sorting";
 const List = (props) => {
     const [list, setList] = useState(null)
     const [page, setPage] = useState(1)
-    const [pageSize, setPageSize] = useState(34)
+    const [pageSize, setPageSize] = useState(3)
+    const [isLoading,setIsLoading] = useState(false)
     const [changePage, setChangePage] = useState(false)
-    const [sortList, setSortList] = useState(null);
-    const [sortMethod, setSortMethod] = useState(false);
-    async function fetchList () {
-        const data = await FetchData.fetchCurrencyList( props.match.params.id || 1 ,pageSize)
-        sortData(sortList, data, sortMethod)
-        const newList = data.map((el, index) => {
-            return <ListItem key={el.id} coin={el}/>
-        })
-        setList(newList)
+    const [sortType, setSortType] = useState('market_cap_rank');
+    const [sortMethod, setSortMethod] = useState(true);
+
+    async function fetchList() {
+        return await FetchData.fetchCurrencyList(props.match.params.id || 1, pageSize)
     }
 
     useEffect(() => {
-
-    }, [])
-
-    useEffect(() => {
-        const pageCount = Math.ceil(250/pageSize)
-        const massive = []
-        for (let i = 0; i < pageCount; i++) {
-            massive.push(<PageList key={i} index={i+1}/>)
+        if(list){
+            setList(sortData(sortType, list, sortMethod))
+        }else{
+            fetchList().then((data) => {
+                setList(data)
+                const pageCount = Math.ceil(250 / pageSize)
+                const massive = []
+                for (let i = 0; i < pageCount; i++) {
+                    massive.push(<PageList key={i} index={i + 1}/>)
+                }
+                setPage(...[massive])
+                setIsLoading(true)
+            })
         }
-        setPage(...[massive])
-        // eslint-disable-next-line
-        fetchList()
-    },[props.match.params.id,changePage])
+
+    }, [props.match.params.id, changePage, sortMethod,sortType])
+
+
 
     return (
-        <div className={'wrapper'}>
-            <header className="header">
-                <div className="container">
-                    <nav className="nav">
-                        <div className="header__pages">
-                            {page}
-                        </div>
-                        <div className='header__input'>
-                            <Input type={'text'} placeholder={'Min: 10'} value={pageSize} setValue={setPageSize}/>
-                            <button className={'header__button'} onClick={() => {
-                                if (Number(pageSize) < 10){
-                                    alert('Error size Min Size 10')
-                                }else{
-                                    setChangePage(!changePage)
-                                }
-                            }}>Change</button>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-            <div className="list">
-                <div className="container">
-                    <div className="list__header">
-                        <div className="list__coin">
-                            Coin
-                        </div>
-                        <div className="list__price">
-                            Price
-                        </div>
-                        <div className="list__volume">
-                            24h volume
-                        </div>
-                        <div className="list__percent">
-                            24h
-                        </div>
-                    </div>
-                    {list}
-                </div>
+      <div className={'wrapper'}>
+          <header className="header">
+
+              <div className="container">
+                  <nav className="nav">
+                      <div className="header__pages">
+                          {page}
+                      </div>
+                      <div className='header__input'>
+                          <Input type={'text'} placeholder={'Min: 10'} value={pageSize} setValue={setPageSize}/>
+                          <button className={'header__button'} onClick={() => {
+                              if (Number(pageSize) < 10) {
+                                  alert('Error size Min Size 10')
+                              } else {
+                                  setChangePage(!changePage)
+                              }
+                          }}>Change
+                          </button>
+                      </div>
+                  </nav>
+              </div>
+          </header>
+          <div className="list">
+              <div className="container">
+                  <div className="list__header">
+                      <div className="list__coin">
+                          Coin
+                      </div>
+                      <div className="list__price" onClick={() => {
+                          setSortType('current_price')
+                          setSortMethod(!sortMethod)
+                      }}>
+                          Price
+                      </div>
+                      <div className="list__volume" onClick={() => {
+                          setSortType('total_volume')
+                          setSortMethod(!sortMethod)
+                      }}>
+                          24h volume
+                      </div>
+                      <div className="list__percent" onClick={() => {
+                          setSortType('price_change_percentage_24h')
+                          setSortMethod(!sortMethod)
+                      }}>
+                          24h
+                      </div>
+                  </div>
+                  {isLoading ? list.map((el) => {
+                      return <ListItem key={el.id} coin={el}/>
+                  }): null}
+              </div>
             </div>
         </div>
     );
